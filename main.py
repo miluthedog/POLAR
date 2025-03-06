9da88f8102495ff4a8b671478650166448f51b40
Pha <anhphamhoang04@gmail.com>
2025-03-06 14:36:18 +0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QComboBox, QPushButton, QLabel, QSizePolicy, QTextEdit, QDialog, QFileDialog, QSlider
 from PyQt6.QtCore import Qt, QPointF
-from PyQt6.QtGui import QIcon, QPainter, QPen, QWheelEvent, QMouseEvent, QColor, QPixmap
+from PyQt6.QtGui import QIcon, QPainter, QPen, QWheelEvent, QMouseEvent, QColor, QPixmap, QImage
 import os
 from modules.camera import camera
 from modules.connect import Arduino
 from modules.firmware import GRBL
-from modules.imageConvert import png2gcode
+from modules.imageConvert import converter
 
 class MainFrontEnd(QWidget):
     def __init__(self):
@@ -25,31 +25,30 @@ class MainFrontEnd(QWidget):
 
     def addLeftWidget(self):
         leftCombos = QHBoxLayout()
-        self.combo1 = QComboBox()
-        self.combo2 = QComboBox()
-        self.combo1.addItems(["COM5", "COM4", "COM3", "COM2", "COM1"])
-        self.combo2.addItem("115200")
-        leftCombos.addWidget(self.combo1)
-        leftCombos.addWidget(self.combo2)
+        self.comboCom, self.comboBaud = QComboBox(), QComboBox()
+        self.comboCom.addItems(["COM5", "COM4", "COM3", "COM2", "COM1"])
+        self.comboBaud.addItem("115200")
+        for combo in (self.comboCom, self.comboBaud):
+            leftCombos.addWidget(combo)
+
 
         leftButtons = QHBoxLayout()
-        self.button1 = QPushButton("Firmware: OFF")
-        self.button2 = QPushButton("Camera: OFF")
-        self.button3 = QPushButton("OFF")
-        self.button1.setFixedSize(90, 30)
-        self.button2.setFixedSize(90, 30)
-        self.button3.setFixedSize(30, 30)
-        self.button1.clicked.connect(self.backend.firmware)
-        self.button2.clicked.connect(self.backend.camera)
-        self.button3.clicked.connect(self.backend.connect)
-        leftButtons.addWidget(self.button1)
-        leftButtons.addWidget(self.button2)
-        leftButtons.addWidget(self.button3)
+        self.buttonFirmware, self.buttonCamera, self.buttonConnect = QPushButton("Firmware: OFF"), QPushButton("Camera: OFF"), QPushButton("OFF")
+        for button in (self.buttonFirmware,self.buttonCamera):
+            button.setFixedSize(90, 30)
+            leftButtons.addWidget(button)
+        self.buttonConnect.setFixedSize(30, 30)
+        leftButtons.addWidget(self.buttonConnect)
+        self.buttonFirmware.clicked.connect(self.backend.firmware)
+        self.buttonCamera.clicked.connect(self.backend.camera)
+        self.buttonConnect.clicked.connect(self.backend.connect)
 
+        leftAreaLayout = QVBoxLayout()
         self.leftArea = QTextEdit()
         self.leftArea.setReadOnly(True)
         self.leftArea.setStyleSheet("background-color: #444444; color: #eeeeee; font-size: 12px; border: 1px solid #00aaaa;")
         self.leftArea.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        leftAreaLayout.addWidget(self.leftArea)
 
         leftMatrix = QGridLayout()
         matrix = []
@@ -66,22 +65,20 @@ class MainFrontEnd(QWidget):
             button.clicked.connect(self.backend.controller)
 
         left = QVBoxLayout()
-        left.addLayout(leftCombos)
-        left.addLayout(leftButtons)
-        left.addWidget(self.leftArea)
-        left.addLayout(leftMatrix)
+        for layout in (leftCombos, leftButtons, leftAreaLayout, leftMatrix):
+            left.addLayout(layout)
         self.leftWidget = QWidget()
         self.leftWidget.setLayout(left)
         self.leftWidget.setFixedWidth(240)
         self.leftWidget.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Expanding)
 
     def addRightWidget(self):
-        buttonBig = QPushButton("Bottom Button")
-        buttonBig.setFixedSize(150, 40)
-        buttonBig.clicked.connect(self.backend.imageConvert)
+        buttonPopup = QPushButton("Bottom Button")
+        buttonPopup.setFixedSize(150, 40)
+        buttonPopup.clicked.connect(self.backend.imageConvert)
 
         rightdown = QVBoxLayout()
-        rightdown.addWidget(buttonBig, alignment=Qt.AlignmentFlag.AlignCenter)
+        rightdown.addWidget(buttonPopup, alignment=Qt.AlignmentFlag.AlignCenter)
         rightdownWidget = QWidget()
         rightdownWidget.setLayout(rightdown)
         rightdownWidget.setFixedHeight(100)
@@ -104,7 +101,7 @@ class MainBackEnd:
         self.popup = None
 
     def firmware(self):
-        firmware = GRBL(self.frontend.combo1.currentText(), self.frontend.combo2.currentText())
+        firmware = GRBL(self.frontend.comboCom.currentText(), self.frontend.comboBaud.currentText())
         
         if not firmware.checkFirmware():
             response = "Firmware not found. Uploading ..."
@@ -116,7 +113,7 @@ class MainBackEnd:
                 self.frontend.leftArea.setText("<br>".join(self.responses))
                 return
 
-        self.frontend.button1.setText("Firmware: ON")
+        self.frontend.buttonFirmware.setText("Firmware: ON")
         response = "Firmware: ready"
         self.responses.append(response)
         self.frontend.leftArea.setText("<br>".join(self.responses))
@@ -125,16 +122,16 @@ class MainBackEnd:
         print("Camera functionality upcoming")
 
     def connect(self):
-        connector = Arduino(self.frontend.combo1.currentText(), self.frontend.combo2.currentText())
+        connector = Arduino(self.frontend.comboCom.currentText(), self.frontend.comboBaud.currentText())
 
-        if self.frontend.button3.text() == "OFF":
+        if self.frontend.buttonConnect.text() == "OFF":
             self.connection, response = connector.connect(self.connection)
             if self.connection:
-                self.frontend.button3.setText("ON")
+                self.frontend.buttonConnect.setText("ON")
         else:
             self.connection, response = connector.disconnect(self.connection)
             if not self.connection:
-                self.frontend.button3.setText("OFF")
+                self.frontend.buttonConnect.setText("OFF")
 
         self.responses.append(response)
         self.frontend.leftArea.setText("<br>".join(self.responses))
@@ -157,22 +154,20 @@ class RightTopFullStack(QWidget):
     def paintEvent(self, event):
         render = QPainter(self)
         render.setRenderHint(QPainter.RenderHint.Antialiasing)
-        
+        bigCyan, smallDcyan, bigDcyan, bigRed = QPen(Qt.GlobalColor.cyan), QPen(Qt.GlobalColor.darkCyan), QPen(Qt.GlobalColor.darkCyan), QPen(Qt.GlobalColor.red)
+        for pen in (bigCyan, bigDcyan, bigRed):
+            pen.setWidth(2)
+        smallDcyan.setWidthF(0.5)
+
         render.fillRect(self.rect(), QColor(44, 44, 44))
-        
-        borderPen = QPen(Qt.GlobalColor.cyan)
-        borderPen.setWidth(2)
-        render.setPen(borderPen)
+
+        render.setPen(bigCyan)
         render.drawRect(self.rect().adjusted(1, 1, -1, -1))
-        
-        grid = QPen(Qt.GlobalColor.darkCyan)
-        grid.setWidthF(0.5)
-        render.setPen(grid)
-        centerX = int(self.width() // 2 + self.offset.x())
-        centerY = int(self.height() // 2 + self.offset.y())
+
+        render.setPen(smallDcyan)
+        centerX, centerY  = int(self.width() // 2 + self.offset.x()), int(self.height() // 2 + self.offset.y())
         for direction in (-1, 1):
-            x = centerX % self.gridSize
-            y = centerY % self.gridSize
+            x, y = centerX % self.gridSize, centerY % self.gridSize
             while 0 < x < self.width():
                 render.drawLine(int(x), 0, int(x), self.height())
                 x += direction * self.gridSize
@@ -180,14 +175,11 @@ class RightTopFullStack(QWidget):
                 render.drawLine(0, int(y), self.width(), int(y))
                 y += direction * self.gridSize
 
-        coord = QPen(Qt.GlobalColor.darkCyan)
-        coord.setWidth(2)
-        render.setPen(coord)
+        render.setPen(bigDcyan)
         render.drawLine(0, centerY, self.width(), centerY)
         render.drawLine(centerX, 0, centerX, self.height())
 
-        origin = QPen(Qt.GlobalColor.red)
-        render.setPen(origin)
+        render.setPen(bigRed)
         render.drawRect(centerX, centerY, 5, 5)
     
     def wheelEvent(self, event: QWheelEvent):
@@ -196,12 +188,10 @@ class RightTopFullStack(QWidget):
 
         if 10 < newgridSize < 100:
             mousePos = event.position()
-            mouseOffsetX = mousePos.x() - self.width() / 2
-            mouseOffsetY = mousePos.y() - self.height() / 2
+            mouseOffsetX, mouseOffsetY = mousePos.x() - self.width() / 2, mousePos.y() - self.height() / 2
             scale = newgridSize / self.gridSize
             self.offset.setX(self.offset.x() * scale + mouseOffsetX * (1 - scale))
             self.offset.setY(self.offset.y() * scale + mouseOffsetY * (1 - scale))
-            
             self.gridSize = newgridSize
             self.update()
     
@@ -211,8 +201,7 @@ class RightTopFullStack(QWidget):
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if self.lastMouse is not None:
-            delta = event.position() - self.lastMouse
-            self.offset += delta
+            self.offset += event.position() - self.lastMouse
             self.lastMouse = event.position()
             self.update()
 
@@ -227,10 +216,7 @@ class PopupFrontEnd(QDialog):
         self.setFixedSize(800, 500)
         self.setWindowTitle("Image Converter")
         self.setWindowIcon(QIcon("polar.ico"))
-
-        self.feedrateValue = 1000
-        self.scaleValue = 1.0
-        self.spacingValue = 5
+        self.feedrateValue, self.scaleValue, self.spacingValue = 1000, 1.0, 5
         
         self.backend = PopupBackEnd(self)
         self.addLeftWidget()
@@ -249,79 +235,85 @@ class PopupFrontEnd(QDialog):
 
     def addRightWidget(self):
         buttonLayout = QHBoxLayout()
-        self.bigButton1 = QPushButton("Load Image", self)
-        self.bigButton2 = QPushButton("Done", self)
-        for bigButton in (self.bigButton1, self.bigButton2):
-            bigButton.setFixedSize(80, 30)
-            buttonLayout.addWidget(bigButton, alignment=Qt.AlignmentFlag.AlignHCenter)
-        self.bigButton1.clicked.connect(self.backend.loadImage)
-        self.bigButton2.clicked.connect(self.backend.doneImage)
+        buttonLoad, buttonDone = QPushButton("Load Image", self), QPushButton("Done", self)
+        for button in (buttonLoad, buttonDone):
+            button.setFixedSize(80, 30)
+            buttonLayout.addWidget(button, alignment=Qt.AlignmentFlag.AlignHCenter)
+        buttonLoad.clicked.connect(self.backend.loadImage)
+        buttonDone.clicked.connect(self.backend.doneImage)
 
         sliderLayout = QHBoxLayout()
-        self.slider1 = QSlider(Qt.Orientation.Vertical)
-        self.slider2 = QSlider(Qt.Orientation.Vertical)
-        self.slider3 = QSlider(Qt.Orientation.Vertical)
-        for slider in (self.slider1, self.slider2, self.slider3):
+        sliderFeedrate, sliderScale, sliderSpacing = QSlider(Qt.Orientation.Vertical), QSlider(Qt.Orientation.Vertical), QSlider(Qt.Orientation.Vertical)
+        for slider in (sliderFeedrate, sliderScale, sliderSpacing):
             slider.setFixedHeight(250)
             slider.setMinimum(1)
             slider.setMaximum(20)
             sliderLayout.addWidget(slider)
-        self.slider1.setValue(20)
-        self.slider2.setValue(10)
-        self.slider3.setValue(5)
-        self.slider1.valueChanged.connect(self.getFeedrate)
-        self.slider2.valueChanged.connect(self.getScale)
-        self.slider3.valueChanged.connect(self.getSpacing)
+        sliderFeedrate.setValue(20)
+        sliderScale.setValue(10)
+        sliderSpacing.setValue(5)
+        sliderFeedrate.valueChanged.connect(self.getFeedrate)
+        sliderScale.valueChanged.connect(self.getScale)
+        sliderSpacing.valueChanged.connect(self.getSpacing)
 
         rowLayout = QVBoxLayout()
-        self.label1 = QLabel(f"Feedrate: {self.feedrateValue}", self)
-        self.label2 = QLabel(f"Size: {self.scaleValue}", self)
-        self.label3 = QLabel(f"Line Spacing: {self.spacingValue}", self)
-        self.button1 = QPushButton("?")
-        self.button2 = QPushButton("?")
-        self.button3 = QPushButton("?")
-        row1 = QHBoxLayout()
-        row2 = QHBoxLayout()
-        row3 = QHBoxLayout()
-        for label, button, row in zip([self.label1, self.label2, self.label3], [self.button1, self.button2, self.button3], [row1, row2, row3]):
+        self.labelFeedrate = QLabel(f"Feedrate: {self.feedrateValue}", self)
+        self.labelScale = QLabel(f"Size: {self.scaleValue}", self)
+        self.labelSpacing = QLabel(f"Line Spacing: {self.spacingValue}", self)
+        buttonFeedrate, buttonScale, buttonSpacing = QPushButton("?"), QPushButton("?"), QPushButton("?")
+        rowFeedrate, rowScale, rowSpacing = QHBoxLayout(), QHBoxLayout(), QHBoxLayout()
+        for label, button, row in zip([self.labelFeedrate, self.labelScale, self.labelSpacing], [buttonFeedrate, buttonScale, buttonSpacing], [rowFeedrate, rowScale, rowSpacing]):
             button.setFixedSize(20, 20)
             row.addWidget(label)
             row.addWidget(button)
             rowLayout.addLayout(row)
+        buttonFeedrate.clicked.connect(self.backend.hintFeedrate)
+        buttonScale.clicked.connect(self.backend.hintScale)
+        buttonSpacing.clicked.connect(self.backend.hintSpacing)
 
         right = QVBoxLayout()
-        right.addLayout(buttonLayout)
-        right.addLayout(sliderLayout)
-        right.addLayout(rowLayout)
+        for layout in (buttonLayout, sliderLayout, rowLayout):
+            right.addLayout(layout)
         self.rightWidget = QWidget()
         self.rightWidget.setLayout(right)
 
     def getFeedrate(self, value):
         self.feedrateValue = value * 50
-        self.label1.setText(f"Feedrate: {self.feedrateValue}")
+        self.labelFeedrate.setText(f"Feedrate: {self.feedrateValue}")
 
     def getScale(self, value):
         self.scaleValue = value / 10
-        self.label2.setText(f"Size: {self.scaleValue:.1f}")
+        self.labelScale.setText(f"Size: {self.scaleValue:.1f}")
 
     def getSpacing(self, value):
         self.spacingValue = value
-        self.label3.setText(f"Line Spacing: {self.spacingValue}")
+        self.labelSpacing.setText(f"Line Spacing: {self.spacingValue}")
 
 class PopupBackEnd:
     def __init__(self, frontend):
         self.frontend = frontend
 
     def loadImage(self):
-        initial_dir = os.path.dirname(os.path.abspath(__file__))
+        initialDir = os.path.dirname(os.path.abspath(__file__))
         pathFilter = "Image Files (*.png *.jpg);;Vector Files (*.svg *.pdf);;All Files (*)"
-        file_path, _ = QFileDialog.getOpenFileName(self.frontend, "Select Image", initial_dir, pathFilter)
-        if file_path:
-            pixmap = QPixmap(file_path)
-            self.frontend.leftWidget.setPixmap(pixmap)
+        filePath, _ = QFileDialog.getOpenFileName(self.frontend, "Select Image", initialDir, pathFilter)
+        image, height, width, bytesLine = converter(filePath).img2croprgba()
+
+        qImage = QImage(image.data, width, height, bytesLine, QImage.Format.Format_RGBA8888)
+        qPixmap = QPixmap.fromImage(qImage)
+        self.frontend.leftWidget.setPixmap(qPixmap)
     
     def doneImage(self):
         self.frontend.accept()
+    
+    def hintFeedrate(self):
+        pass
+    
+    def hintScale(self):
+        pass
+    
+    def hintSpacing(self):
+        pass
 
 if __name__ == "__main__":
     app = QApplication([])
--- modules/imageConvert.py
+++ modules/imageConvert.py
@@ -1,6 +1,20 @@
+import cv2 as cv
 
 
+class converter():
+    def __init__(self, filePath):
+        self.filePath = filePath
 
-class png2gcode():
-    def __init__(self):
-        pass
+    def img2croprgba(self):
+        inImage = cv.imread(self.filePath, cv.IMREAD_GRAYSCALE)
+        _,white = cv.threshold(inImage, 240, 255, cv.THRESH_BINARY)
+        black = cv.bitwise_not(white)
+        coords = cv.findNonZero(black)
+        x, y, w, h = cv.boundingRect(coords)
+        cropped_image = inImage[y:y+h, x:x+w]
+        cropped_black = black[y:y+h, x:x+w]
+        outImage = cv.cvtColor(cropped_image, cv.COLOR_GRAY2BGRA)
+        outImage[:, :, 3] = cropped_black
+        height, width, channel = outImage.shape
+        bytesLine = channel * width
+        return outImage, height, width, bytesLine
